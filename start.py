93d1e9a69176a7338c33f70a5cb97a2823e039a7
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-04-20 18:58:38 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 from os import system # chala ja 
-system ("git clone https://GitHub.com/HACKERBOTTELEGRAM/HACKERBOTOP && cd HACKERBOTOP && python3 -m userbot")
+system ("git clone https://GitHub.com/TeamDynamic/Dynamic-Userbot && cd Dynamic-Userbot && python3 -m DYNAMIC")
 # try af
 print("OH SYSTEM IS CRASHED")
-system("python3 -m userbot")
+system("python3 -m DYNAMIC")
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @DARKLON_USERBOT_SUPPORT")
