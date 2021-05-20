8a0698f51f85a528d3128496203479cdc6336cb8
Team Dynamic <72003733+amanpandey7647@users.noreply.github.com>
2021-05-20 10:22:39 +0530

    done

--- start.py
+++ start.py
@@ -1,6 +1,3 @@
 from os import system # chala ja 
 system ("git clone https://GitHub.com/TeamDynamic/Dynamic-Userbot && cd Dynamic-Userbot && python3 -m DYNAMIC")
-# try af
-print("There Is A Problem In Userbot")
 system("python3 -m DYNAMIC")
-print ("You Have Error In Your Userbot Check If You Are A Owner Or Dev OR Join For Support @DYNAMICUSERBOTSUPPORT")
