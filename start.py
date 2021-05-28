78ef8b3e3198bff7a7ca2f89458780fa589ea3d9
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-05-28 11:10:54 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,3 +1,4 @@
 from os import system # chala ja 
 system ("git clone https://GitHub.com/TeamDynamic/Dynamic-Userbot && cd Dynamic-Userbot && python3 -m DYNAMIC")
 system("python3 -m DYNAMIC")
+echo " EXTREME PRO STARTING "
